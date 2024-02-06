4cc0edc71cc196ed310e6833528edac1f753cec6
Kinfe123 <kinfishtech@gmail.com>
2024-02-06 16:23:28 +0300

    added center wrapper

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignIn } from "@clerk/nextjs";
  
 export default function Page() {
-  return <SignIn />;
+  return  (
+    <div className="flex justify-center items-center translate-y-1/2">
+        <SignIn />
+    </div>
+  )
+    
 }
