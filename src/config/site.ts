c361b68ec43de79e916c0a796426412b8cabdcab
Kinfe123 <kinfishtech@gmail.com>
2024-02-07 10:23:06 +0300

    mat data fix

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,17 +3,16 @@ import { SiteConfig } from "@/types"
 import { env } from "@/env.mjs"
 
 export const siteConfig: SiteConfig = {
-  name: "Next Entree",
-  author: "redpangilinan",
-  description:
-    "Next.js 14+ starter template with app router, shadcn/ui, typesafe env, icons and configs setup.",
+  name: "ChaCha",
+  author: "Kinfish",
+  description: "Streaming service based on Ethiopia",
   keywords: ["Next.js", "React", "Tailwind CSS", "Radix UI", "shadcn/ui"],
   url: {
     base: env.NEXT_PUBLIC_APP_URL,
-    author: "https://rdev.pro",
+    author: "https://kinfish-dumpy.vercel.app",
   },
   links: {
-    github: "https://github.com/redpangilinan/next-entree",
+    github: "https://github.com/Kinfe123/chacha",
   },
   ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.jpg`,
 }
