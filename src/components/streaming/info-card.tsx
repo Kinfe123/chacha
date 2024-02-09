01160c1ab77335434c89623ab35b75e810540f6b
Kinfe123 <kinfishtech@gmail.com>
2024-02-09 23:02:31 +0300

    edited a edit page

--- src/components/streaming/info-card.tsx
+++ src/components/streaming/info-card.tsx
@@ -1,18 +1,18 @@
-"use client";
+"use client"
 
-import Image from "next/image";
-import { Pencil } from "lucide-react";
+import Image from "next/image"
+import { Pencil } from "lucide-react"
 
-import { Separator } from "@/components/ui/separator";
+import { Separator } from "@/components/ui/separator"
 
-import { InfoModal } from "./info-modal";
+import { InfoModal } from "./info-modal"
 
 interface InfoCardProps {
-  name: string;
-  thumbnailUrl: string | null;
-  hostIdentity: string;
-  viewerIdentity: string;
-};
+  name: string
+  thumbnailUrl: string | null
+  hostIdentity: string
+  viewerIdentity: string
+}
 
 export const InfoCard = ({
   name,
@@ -20,47 +20,38 @@ export const InfoCard = ({
   hostIdentity,
   viewerIdentity,
 }: InfoCardProps) => {
-  const hostAsViewer = `host-${hostIdentity}`;
-  const isHost = viewerIdentity === hostAsViewer;
+  const hostAsViewer = `host-${hostIdentity}`
+  const isHost = viewerIdentity === hostAsViewer
 
-  if (!isHost) return null;
+  if (!isHost) return null
 
   return (
     <div className="px-4">
       <div className="rounded-xl bg-background">
         <div className="flex items-center gap-x-2.5 p-4">
-          <div className="rounded-md bg-blue-600 p-2 h-auto w-auto">
+          <div className="h-auto w-auto rounded-md border-2 border-white/90 bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,100,210,0.3),rgba(255,255,255,0))] p-2">
             <Pencil className="h-5 w-5" />
           </div>
           <div>
-            <h2 className="text-sm lg:text-lg font-semibold capitalize">
+            <h2 className="text-sm font-semibold capitalize lg:text-lg">
               Edit your stream info
             </h2>
-            <p className="text-muted-foreground text-xs lg:text-sm">
+            <p className="text-xs text-muted-foreground lg:text-sm">
               Maximize your visibility
             </p>
           </div>
-          <InfoModal
-            initialName={name}
-            initialThumbnailUrl={thumbnailUrl}
-          />
+          <InfoModal initialName={name} initialThumbnailUrl={thumbnailUrl} />
         </div>
         <Separator />
-        <div className="p-4 lg:p-6 space-y-4">
+        <div className="space-y-4 p-4 lg:p-6">
           <div>
-            <h3 className="text-sm text-muted-foreground mb-2">
-              Name
-            </h3>
-            <p className="text-sm font-semibold">
-              {name}
-            </p>
+            <h3 className="mb-2 text-sm text-muted-foreground">Name</h3>
+            <p className="text-sm font-semibold">{name}</p>
           </div>
           <div>
-            <h3 className="text-sm text-muted-foreground mb-2">
-              Thumbnail
-            </h3>
+            <h3 className="mb-2 text-sm text-muted-foreground">Thumbnail</h3>
             {thumbnailUrl && (
-              <div className="relative aspect-video rounded-md overflow-hidden w-[200px] border border-white/10">
+              <div className="relative aspect-video w-[200px] overflow-hidden rounded-md border border-white/10">
                 <Image
                   fill
                   src={thumbnailUrl}
@@ -73,5 +64,5 @@ export const InfoCard = ({
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
