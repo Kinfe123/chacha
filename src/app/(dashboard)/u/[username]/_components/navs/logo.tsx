ca8e7e1e2c7fca1dc17bb04a93549d2cf2b8a688
Kinfe123 <kinfishtech@gmail.com>
2024-02-14 17:52:48 +0300

    fix: cooking it

--- src/app/(dashboard)/u/[username]/_components/navs/logo.tsx
+++ src/app/(dashboard)/u/[username]/_components/navs/logo.tsx
@@ -9,16 +9,16 @@ export const Logo = () => {
     <Link href="/">
       <div className="flex items-center gap-x-4 hover:opacity-75 transition">
         <div className="bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink">
-          <Image
+          {/* <Image
             src="/spooky.svg"
             alt="Gamehub"
             height="32"
             width="32"
-          />
+          /> */}
         </div>
         <div className={cn(
           "hidden lg:block",
-      
+
         )}>
           <p className="text-lg font-semibold">
             ChaCha
