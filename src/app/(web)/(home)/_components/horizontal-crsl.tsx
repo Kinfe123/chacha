c92989445d2b026eb6ce22fbd04204d015c3f349
Kinfe123 <kinfishtech@gmail.com>
2024-02-14 22:51:24 +0300

    crsl added

---
+++ src/app/(web)/(home)/_components/horizontal-crsl.tsx
@@ -0,0 +1,108 @@
+'use client'
+
+import { motion, useTransform, useScroll } from "framer-motion";
+import { useRef } from "react";
+import FuzzyOverlay from "./fuzzy";
+
+const Example = () => {
+  return (
+    <div className="bg-transparent overflow-hidden">
+      {/* <div className="flex h-48 items-center justify-center">
+        <span className="font-semibold uppercase text-neutral-500">
+          Scroll down
+        </span>
+      </div> */}
+      <HorizontalScrollCarousel />
+      <div className="flex h-48 items-center justify-center">
+        {/* <span className="font-semibold uppercase text-neutral-500">
+          Scroll up
+        </span> */}
+      </div>
+    </div>
+  );
+};
+
+export const HorizontalScrollCarousel = () => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  });
+
+  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
+
+  return (
+    <section ref={targetRef} className="relative h-[300vh] bg-transparent w-screen ">
+      <div className="sticky top-0 flex h-screen items-center overflow-hidden ">
+        <motion.div style={{ x }} className="flex gap-4 ">
+          {cards.map((card) => {
+            return <Card card={card} key={card.id} />;
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+// @ts-ignore
+const Card = ({ card }) => {
+  return (
+    <div
+      key={card.id}
+      className="group relative h-[450px] w-[450px] overflow-hidden rounded-3xl bg-transparent border-1 border-white"
+    >
+        <FuzzyOverlay />
+      <div
+        style={{
+          backgroundImage: `url(${card.url})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className="absolute inset-0 z-0 transition-transform duration-300 group-hover:scale-110"
+      ></div>
+      <div className="absolute inset-0 z-10 grid place-content-center">
+        <p className="bg-gradient-to-br font-heading2 from-white/20 to-white/0 p-8 text-6xl font-black uppercase text-white backdrop-blur-lg">
+          {card.title}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Example;
+
+const cards = [
+  {
+    url: "/imgs/abstract/1.jpg",
+    title: "Title 1",
+    id: 1,
+  },
+  {
+    url: "/imgs/abstract/2.jpg",
+    title: "Title 2",
+    id: 2,
+  },
+  {
+    url: "/imgs/abstract/3.jpg",
+    title: "Title 3",
+    id: 3,
+  },
+  {
+    url: "/imgs/abstract/4.jpg",
+    title: "Title 4",
+    id: 4,
+  },
+  {
+    url: "/imgs/abstract/5.jpg",
+    title: "Title 5",
+    id: 5,
+  },
+  {
+    url: "/imgs/abstract/6.jpg",
+    title: "Title 6",
+    id: 6,
+  },
+  {
+    url: "/imgs/abstract/7.jpg",
+    title: "Title 7",
+    id: 7,
+  },
+];
--- src/app/(web)/(home)/page.tsx
+++ src/app/(web)/(home)/page.tsx
@@ -1,14 +1,26 @@
 import ForFree from "./_components/for-free"
 import GetStarted from "./_components/get-started"
 import Hero from "./_components/hero"
+import { HorizontalScrollCarousel } from "./_components/horizontal-crsl"
 import World from "./_components/people-around"
 
 const LandingPage = () => {
   return (
-    <div className="overflow-hidden">
+    <div className="">
       <Hero />
       <div className="relative">
         <GetStarted />
+      </div>
+
+      <div className=" grid grid-cols-2 gap-1 bg-neutral-950/50 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(217,176,225,0.12),rgba(255,255,255,0))]">
+        <div className="mt-[-500x] flex   justify-center items-center bg-transparent px-10">
+          <p className="d:text-4xl mt-[-700px] font-heading lg:text-6xl xl:text-[12rem] font-medium bg-gradient-to-tr from-[#E0B379] to-[#e8eaed] bg-clip-text text-transparent">
+            We Grow <br /> 
+          </p>
+        </div>
+        <div className="overflow-clip">
+          <HorizontalScrollCarousel />
+        </div>
       </div>
       <div className="relative">
         <World />
