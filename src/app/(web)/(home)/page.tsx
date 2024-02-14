c8924e6b6c0e9607504dce8c24f0edf98af86524
Kinfe123 <kinfishtech@gmail.com>
2024-02-14 23:24:40 +0300

    fix: change color palette

--- src/app/(web)/(home)/page.tsx
+++ src/app/(web)/(home)/page.tsx
@@ -14,7 +14,7 @@ const LandingPage = () => {
 
       <div className=" grid grid-cols-2 gap-1 bg-neutral-950/50 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(217,176,225,0.12),rgba(255,255,255,0))]">
         <div className="mt-[-500x] flex   justify-center items-center bg-transparent px-10">
-          <p className="md:text-4xl mt-[-700px] font-heading lg:text-6xl xl:text-[12rem] font-medium bg-gradient-to-tr from-[#E0B379] to-[#e8eaed] bg-clip-text text-transparent">
+          <p className="md:text-4xl mt-[-700px] font-heading lg:text-6xl xl:text-[12rem] font-medium bg-gradient-to-tr from-purple-400 to-red-200  bg-clip-text text-transparent">
             We help <br />  you grow from beginning.
           </p>
         </div>
