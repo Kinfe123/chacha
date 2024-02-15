e5736c9b05212d27b61674da3f136e1865815b6b
Kinfe123 <kinfishtech@gmail.com>
2024-02-16 01:00:47 +0300

    fix: edgecase not found for tnx

--- src/components/transaction-complete.tsx
+++ src/components/transaction-complete.tsx
@@ -38,7 +38,7 @@ export function TransactionComplete({ first_name, last_name, email, amount, tnx_
       </div>
       <Link
         className="flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-        href="#"
+        href="/streams"
       >
         Go to Dashboard
       </Link>
