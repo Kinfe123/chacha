1f955d6f165b72a11c1a5e2fa524d036bfadbe82
Kinfe123 <kinfishtech@gmail.com>
2024-02-15 17:01:17 +0300

    api added for chapa

---
+++ src/app/api/chapa/route.ts
@@ -0,0 +1,61 @@
+type CustomizationType = {
+  title: string
+  description: string
+}
+
+type PaymentAcceptProps = {
+  amount: string
+  currency: string
+  email: string
+  first_name: string
+  last_name: string
+  phone_number: string
+  tx_ref: string
+  callback_url: string
+  return_url: string
+  customization: CustomizationType
+}
+export async function GET(req: Request) {
+  return new Response("Chapa endpoints")
+}
+export async function POST(req: Request) {
+  const {
+    amount,
+    currency,
+    email,
+    first_name,
+    last_name,
+    phone_number,
+    tx_ref,
+    callback_url,
+    return_url,
+    customization,
+  }  = (req.body) as unknown as PaymentAcceptProps
+
+  console.log('THe name and the stuff here are logged : ' , amount , currency , first_name , last_name )
+  try {
+    const header = {
+        headers: { 
+            Authorization: `Bearer ${process.env.CHAPA_SECRET_KEY}`,
+            "Content-Type": "application/json"
+         },
+        
+    };
+    const body = {
+        amount: amount,
+        currency: currency,
+        email: email,
+        first_name: first_name,
+        last_name: last_name,
+        phone_number: phone_number,
+        tx_ref: tx_ref,
+        callback_url: callback_url,
+        return_url: return_url,
+                 "customization[title]" : customization.title,
+                 "customization[description]" : customization.description
+       }
+
+  } catch (err) {
+    console.log("Error has occured [CHAPA]", err)
+  }
+}
