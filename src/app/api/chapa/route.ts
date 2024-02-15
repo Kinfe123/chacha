936dc753e8f136646fbd75881a2f12f6b52f7fe2
Kinfe123 <kinfishtech@gmail.com>
2024-02-15 17:14:47 +0300

    generation and chapa api

--- src/app/api/chapa/route.ts
+++ src/app/api/chapa/route.ts
@@ -1,80 +1,85 @@
 import axios from 'axios'
 
 type CustomizationType = {
-  title: string
-  description: string
+    title: string
+    description: string
 }
 
 type PaymentAcceptProps = {
-  amount: string
-  currency: string
-  email: string
-  first_name: string
-  last_name: string
-  phone_number: string
-  tx_ref: string
-  callback_url: string
-  return_url: string
-  customization: CustomizationType
+    amount: string
+    currency: string
+    email: string
+    first_name: string
+    last_name: string
+    phone_number: string
+    tx_ref: string
+    callback_url: string
+    return_url: string
+    customization: CustomizationType
 }
 export async function GET(req: Request) {
-  return new Response("Chapa endpoints")
+    return new Response("Chapa endpoints")
 }
 export async function POST(req: Request) {
-  const {
-    amount,
-    currency,
-    email,
-    first_name,
-    last_name,
-    phone_number,
-    tx_ref,
-    callback_url,
-    return_url,
-    customization,
-  }  = (req.body) as unknown as PaymentAcceptProps
+    const {
+        amount,
+        currency,
+        email,
+        first_name,
+        last_name,
+        phone_number,
+        tx_ref,
+        callback_url,
+        return_url,
+        customization,
+    } = (req.body) as unknown as PaymentAcceptProps
 
-  console.log('THe name and the stuff here are logged : ' , amount , currency , first_name , last_name )
-  try {
-    const header = {
-        headers: { 
-            Authorization: `Bearer ${process.env.CHAPA_SECRET_KEY}`,
-            "Content-Type": "application/json"
-         },
-        
-    };
-    const body = {
-        amount: amount,
-        currency: currency,
-        email: email,
-        first_name: first_name,
-        last_name: last_name,
-        phone_number: phone_number,
-        tx_ref: tx_ref,
-        callback_url: callback_url,
-        return_url: return_url,
-                 "customization[title]" : customization.title,
-                 "customization[description]" : customization.description
-       }
+    console.log('THe name and the stuff here are logged : ', amount, currency, first_name, last_name)
+    try {
+        const header = {
+            headers: {
+                Authorization: `Bearer ${process.env.CHAPA_SECRET_KEY}`,
+                "Content-Type": "application/json"
+            },
 
-       let resp = "";
-       // getting the data back from the intiation 
-       try {
+        };
+        const body = {
+            amount: amount,
+            currency: currency,
+            email: email,
+            first_name: first_name,
+            last_name: last_name,
+            phone_number: phone_number,
+            tx_ref: tx_ref,
+            callback_url: callback_url,
+            return_url: return_url,
+            "customization[title]": customization.title,
+            "customization[description]": customization.description
+        }
 
-           const req = await axios.post("https://api.chapa.co/v1/transaction/initialize")
-           const response = await req.data
+        let resp = "";
+        // getting the data back from the intiation 
+        try {
+
+            const req = await axios.post("https://api.chapa.co/v1/transaction/initialize")
+            const response = await req.data
             resp = response
+            return new Response(JSON.stringify(resp), { status: 200 })
 
-       }catch(err){
-           console.log("Error has occured on [CHAPA_INIT] ")
-       }
-              
-       
-    //    await axios.post("https://api.chapa.co/v1/transaction/initialize", body, header).then(response => {
-    //      resp = response;
-    //    })
 
-  } catch (err) {
-    console.log("Error has occured [CHAPA]", err)
-  }
+
+        } catch (err) {
+            console.log("Error has occured on [CHAPA_INIT] ")
+            return new Response(JSON.stringify(err) , {status:400})
+        }
+
+
+        //    await axios.post("https://api.chapa.co/v1/transaction/initialize", body, header).then(response => {
+        //      resp = response;
+        //    })
+
+    } catch (err) {
+        console.log("Error has occured [CHAPA]", err)
+        return new Response(JSON.stringify(err) , {status:400})
+    }
 }
