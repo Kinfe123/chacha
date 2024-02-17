9f834c233de89a62da50dc8b3aa5d206d0f849ed
Kinfe123 <kinfishtech@gmail.com>
2024-02-17 22:36:44 +0300

    on collapse updated

--- src/app/(dashboard)/u/[username]/_components/asides/items.tsx
+++ src/app/(dashboard)/u/[username]/_components/asides/items.tsx
@@ -48,7 +48,7 @@ export const NavItem = ({
             <Hint asChild label={label}>
               <div className="relative">
 
-                {label === '1 time Messages' ? <p className="font-bold absolute top-[-15px] right-[-10px] w-4 h-4 bg-white rounded-full text-black text-center text-[1.10rem]">{isNewMsg}</p> : ''}
+                {label === '1 time Messages' ? <p className="font-bold absolute top-[-13px] right-[-10px] w-4 h-4 bg-white rounded-full text-black text-center text-[1.10rem]">{isNewMsg}</p> : ''}
                 <Icon className={cn(
                   "h-4 w-4",
                   collapsed ? "mr-0" : "mr-2"
