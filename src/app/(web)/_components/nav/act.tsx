b1f357db1c5c383d35681ce4ef69c128e18cff6a
Kinfe123 <kinfishtech@gmail.com>
2024-02-20 22:11:33 +0300

    cleaning up lucide 01

--- src/app/(web)/_components/nav/act.tsx
+++ src/app/(web)/_components/nav/act.tsx
@@ -7,14 +7,13 @@ import {
 } from "@clerk/nextjs";
 
 import { Button } from "@/components/ui/button";
-import { ModeToggle } from "@/components/mode-toggle";
+
 
 import UserNav from "@/components/user-nav";
 
 export const Actions = async () => {
   const user = await currentUser();
-  const initials = `${user?.firstName?.charAt(0) ?? ""} ${user?.lastName?.charAt(0) ?? ""
-    }`
+
 
   return (
     <div className="flex items-center justify-end gap-x-2 ml-4 lg:ml-0">
