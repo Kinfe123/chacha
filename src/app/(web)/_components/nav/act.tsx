645247029531a98f14311f5594de30c69048f454
Kinfe123 <kinfishtech@gmail.com>
2024-02-20 19:38:33 +0300

    init account haader added

--- src/app/(web)/_components/nav/act.tsx
+++ src/app/(web)/_components/nav/act.tsx
@@ -1,16 +1,21 @@
 import Link from "next/link";
-import { Clapperboard } from "lucide-react";
-import { 
-  SignInButton, 
-  UserButton, 
+import { Clapperboard, CreditCard, LayoutDashboard, LogOut, Settings } from "lucide-react";
+import {
+  SignInButton,
+  UserButton,
   currentUser
 } from "@clerk/nextjs";
 
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toggle";
+import { DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger, DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut } from "@/components/ui/dropdown-menu";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export const Actions = async () => {
   const user = await currentUser();
+  const initials = `${user?.firstName?.charAt(0) ?? ""} ${user?.lastName?.charAt(0) ?? ""
+    }`
 
   return (
     <div className="flex items-center justify-end gap-x-2 ml-4 lg:ml-0">
@@ -37,9 +42,73 @@ export const Actions = async () => {
               </span>
             </Link>
           </Button>
-          <UserButton
-            afterSignOutUrl="/"
-          />
+          {user && (<DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="secondary"
+                className="relative size-8 rounded-full"
+              >
+                <Avatar className="size-8">
+                  <AvatarImage
+                    src={user.imageUrl}
+                    alt={user.username ?? ""}
+                  />
+                  <AvatarFallback>{initials}</AvatarFallback>
+                </Avatar>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56" align="end" forceMount>
+              <DropdownMenuLabel className="font-normal">
+                <div className="flex flex-col space-y-1">
+                  <p className="text-sm font-medium leading-none">
+                    {user.firstName} {user.lastName}
+                  </p>
+                  <p className="text-xs leading-none text-muted-foreground">
+                    {user.emailAddresses[0].emailAddress}
+                  </p>
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem asChild>
+                  <Link href="/dashboard/stores">
+                    <LayoutDashboard
+                      className="mr-2 size-4"
+                      aria-hidden="true"
+                    />
+                    Dashboard
+                    <DropdownMenuShortcut>⌘D</DropdownMenuShortcut>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                  <Link href="/dashboard/billing">
+                    <CreditCard
+                      className="mr-2 size-4"
+                      aria-hidden="true"
+                    />
+                    Billing
+                    <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                  <Link href="/dashboard/account">
+                    <Settings className="mr-2 size-4" aria-hidden="true" />
+                    Settings
+                    <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem asChild>
+                <Link href="/signout">
+                  <LogOut className="mr-2 size-4" aria-hidden="true" />
+                  Log out
+                  <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>)}
+
         </div>
       )}
     </div>
