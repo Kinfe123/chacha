d361cfb702f2ab5d27fa79ed8aa6c8d727d05c2f
Kinfe123 <kinfishtech@gmail.com>
2024-02-20 22:15:30 +0300

    added signout page

--- src/app/(web)/(home)/signout/_components/logout-btn.tsx
+++ src/app/(web)/(home)/signout/_components/logout-btn.tsx
@@ -12,52 +12,52 @@ import { Skeleton } from "@/components/ui/skeleton"
 import { Loader } from "lucide-react"
 
 export function LogOutButtons() {
-  const router = useRouter()
-  const mounted = useMounted()
-  const [isPending, startTransition] = React.useTransition()
+    const router = useRouter()
+    const mounted = useMounted()
+    const [isPending, startTransition] = React.useTransition()
 
-  return (
-    <div className="flex w-full items-center space-x-2">
-      {mounted ? (
-        <SignOutButton
-          signOutCallback={() =>
-            startTransition(() => {
-              router.push(`${window.location.origin}/?redirect=false`)
-            })
-          }
-        >
-          <Button
-            aria-label="Log out"
-            size="sm"
-            className="w-full"
-            disabled={isPending}
-          >
-            {isPending && (
-              <Loader className="mr-2 size-4 animate-spin" />
+    return (
+        <div className="flex max-w-3xl mx-auto  items-center space-x-2">
+            {mounted ? (
+                <SignOutButton
+                    signOutCallback={() =>
+                        startTransition(() => {
+                            router.push(`${window.location.origin}/?redirect=false`)
+                        })
+                    }
+                >
+                    <Button
+                        aria-label="Log out"
+                        size="sm"
+                        className="w-full"
+                        disabled={isPending}
+                    >
+                        {isPending && (
+                            <Loader className="mr-2 size-4 animate-spin" />
+                        )}
+                        Log out
+                    </Button>
+                </SignOutButton>
+            ) : (
+                <Skeleton
+                    className={cn(
+                        buttonVariants({ size: "sm" }),
+                        "w-full bg-muted text-muted-foreground"
+                    )}
+                >
+                    Log out
+                </Skeleton>
             )}
-            Log out
-          </Button>
-        </SignOutButton>
-      ) : (
-        <Skeleton
-          className={cn(
-            buttonVariants({ size: "sm" }),
-            "w-full bg-muted text-muted-foreground"
-          )}
-        >
-          Log out
-        </Skeleton>
-      )}
-      <Button
-        aria-label="Go back to the previous page"
-        variant="outline"
-        size="sm"
-        className="w-full"
-        onClick={() => router.back()}
-        disabled={isPending}
-      >
-        Go back
-      </Button>
-    </div>
-  )
+            <Button
+                aria-label="Go back to the previous page"
+                variant="ghost"
+                size="sm"
+                className="w-full"
+                onClick={() => router.back()}
+                disabled={isPending}
+            >
+                Go back
+            </Button>
+        </div>
+    )
 }
--- src/app/(web)/(home)/signout/page.tsx
+++ src/app/(web)/(home)/signout/page.tsx
@@ -3,32 +3,32 @@ import { env } from "@/env.mjs"
 
 import { LogOutButtons } from "./_components/logout-btn"
 import {
-  PageHeader,
-  PageHeaderDescription,
-  PageHeaderHeading,
+    PageHeader,
+    PageHeaderDescription,
+    PageHeaderHeading,
 } from "@/components/page-header"
 import { Shell } from "@/components/shell"
 
 export const metadata: Metadata = {
-  metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
-  title: "Sign out",
-  description: "Sign out of your account",
+    metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
+    title: "Sign out",
+    description: "Sign out of your account",
 }
 
 export default function SignOutPage() {
-  return (
-    <Shell className="max-w-xs">
-      <PageHeader
-        id="sign-out-page-header"
-        aria-labelledby="sign-out-page-header-heading"
-        className="text-center"
-      >
-        <PageHeaderHeading size="sm">Sign out</PageHeaderHeading>
-        <PageHeaderDescription size="sm">
-          Are you sure you want to sign out?
-        </PageHeaderDescription>
-      </PageHeader>
-      <LogOutButtons />
-    </Shell>
-  )
+    return (
+        <Shell className="max-w-4xl h-full flex justify-center items-center flex-col">
+            <PageHeader
+                id="sign-out-page-header"
+                aria-labelledby="sign-out-page-header-heading"
+                className="text-center"
+            >
+                <PageHeaderHeading size="lg">Sign out</PageHeaderHeading>
+                <PageHeaderDescription size="lg">
+                    Are you sure you want to sign out?
+                </PageHeaderDescription>
+            </PageHeader>
+            <LogOutButtons />
+        </Shell>
+    )
 }
