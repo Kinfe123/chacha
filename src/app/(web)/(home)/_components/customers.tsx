33f3c56556ea5ef5ef5ebcddea10f15ab888e460
Kinfe123 <kinfishtech@gmail.com>
2024-02-26 22:55:32 +0300

    fastening it

--- src/app/(web)/(home)/_components/customers.tsx
+++ src/app/(web)/(home)/_components/customers.tsx
@@ -36,7 +36,7 @@ export function Customers2() {
             <InfiniteMovingCards
                 items={rest}
                 direction="left"
-                speed="slow"
+                speed="fast"
             />
         </div>
     );
