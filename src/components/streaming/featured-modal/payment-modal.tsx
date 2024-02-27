e4872488a0a838b752008c9028e31668d6ed1347
Kinfe123 <kinfishtech@gmail.com>
2024-02-27 12:53:51 +0300

    hint trigger fix

--- src/components/streaming/featured-modal/payment-modal.tsx
+++ src/components/streaming/featured-modal/payment-modal.tsx
@@ -13,26 +13,25 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Hint } from "@/components/hint"
+
 import { PaymentForm } from "./payment-form"
 
 interface PaymentModalProps {
-  name: string,
+  name: string
   disabled?: boolean
 }
 
 const PaymentModal = ({ name, disabled }: PaymentModalProps) => {
   const closeRef = useRef<ElementRef<"button">>(null)
 
-
-
   return (
     <div className="flex h-full items-center justify-center">
       <Dialog>
         <DialogTrigger asChild>
           <Button variant="ghost" disabled={disabled}>
-       
+            <Hint label="Throw them some $">
               <DollarSign className="h-4 w-4 " />
-       
+            </Hint>
           </Button>
         </DialogTrigger>
         <DialogContent className="bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,100,210,0.3),rgba(255,255,255,0))]">
@@ -44,7 +43,7 @@ const PaymentModal = ({ name, disabled }: PaymentModalProps) => {
           </DialogHeader>
 
           <div>
-            <PaymentForm username={name}/>
+            <PaymentForm username={name} />
           </div>
           <div className="flex justify-between">
             <DialogClose ref={closeRef} asChild>
