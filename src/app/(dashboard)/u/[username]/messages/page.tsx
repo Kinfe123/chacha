690f8f8f676750b99a36cfd841d9ed194b225f14
Kinfe123 <kinfishtech@gmail.com>
2024-02-28 14:22:07 +0300

    fix: typo fix

--- src/app/(dashboard)/u/[username]/messages/page.tsx
+++ src/app/(dashboard)/u/[username]/messages/page.tsx
@@ -27,7 +27,16 @@ const Messages = async () => {
                 </PageHeaderDescription>
             </PageHeader>
             <div className="w-full flex flex-wrap gap-3 items-center">
+                {!messages && (
+                    <div className="h-full flex flex-col justify-center items-center">
+                        <h1 className="text-3xl font-bold font-heading md:text-5xl">No Messages :)</h1>
+                        <p className="mx-auto max-w-[500px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                            You can hit up on  <span className="underline underline-offset-1">streams</span>  to make your self as bold as possible
 
+                        </p>
+
+                    </div>
+                )}
                 {messages.map((r) => {
 
                     return (<MessageShow key={r.id} msgid={r.id} msg={r.msg} senderName={r.senderName} senderId={r.senderId} read={r.read} senTime={r.createdAt} />)
